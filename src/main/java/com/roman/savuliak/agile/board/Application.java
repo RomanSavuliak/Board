a075b8f4832c42968122741997ada8055a1a1f00
Roman <rsavulyak@gmail.com>
2018-02-06 17:45:58 +0200

    create project structure

---
+++ src/main/java/com/roman/savuliak/agile/board/Application.java
@@ -0,0 +1,14 @@
+package com.roman.savuliak.agile.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Roman on 06.02.2018.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
